2e05db2c5c5489a01b797c879f73d6d02e301099
UYÊN LÊ THỊ PHƯƠNG <uyen.le@stud.ubbcluj.ro>
2025-12-15 19:49:13 +0200

    Finalizare laborator 1#

---
+++ Controllers/UyenController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uyen_LeThiPhuong_Lab1.Controllers
+{
+    public class UyenController1 : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit!";
+        }
+
+       
+        public string Hello()
+        {
+            return "Salut!";
+        }
+
+  
+        public string Mesajpersonalizat(string nume, int varsta)
+        {
+            return $"Salut {nume} ai varsta de {varsta} ani.";
+        }
+    }
+}
